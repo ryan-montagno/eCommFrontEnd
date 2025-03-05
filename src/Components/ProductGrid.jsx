15c0c029a71f0be7cf94918e9c9009eac3f872ca
Ryan Montagno <ryan.montagno@outlook.com>
2025-03-04 20:36:56 -0500

    Tue Mar  4 08:36:56 PM EST 2025

--- src/Components/ProductGrid.jsx
+++ src/Components/ProductGrid.jsx
@@ -20,17 +20,13 @@ const ProductGrid = () => {
 
   const [selectedProduct, setSelectedProduct] = useState(null);
 
-  const handleCardClick = (product) => {
-    setSelectedProduct(product);
-  };
-
   return (
     <div className="product-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
       {data && data.length > 0 ? (
         data.map((product) => (
           <div
             key={product.product_id}
-            className="product-card bg-white border border-gray-200 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer"
+            className="product-card bg-white border border-gray-200 p-1 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer"
             onClick={() =>
               setSelectedProduct(
                 selectedProduct &&
@@ -42,21 +38,28 @@ const ProductGrid = () => {
           >
             {selectedProduct &&
             selectedProduct.product_id === product.product_id ? (
-              <div className="p-4">
-                <h2 className="text-2xl font-semibold mb-2 text-gray-800">
+              <div className="p-4 flex flex-col items-start relative">
+                <h2 className="text-4xl font-semibold text-gray-800">
                   {product.product_name}
                 </h2>
-                <p className="text-gray-600 mb-4">{product.product_desc}</p>
-                <p className="text-gray-900 font-bold mb-2">
+                <p className="text-md text-gray-600 mb-2">
+                  {product.product_desc}
+                </p>
+                <p className="text-sm text-gray-500 mb-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <p className="text-xs text-gray-500">
+                  Tag: {product.product_tag}
+                </p>
+                <p className="text-sm text-gray-900 font-bold absolute top-4 right-4">
                   ${product.product_price}
                 </p>
-                <p className="text-gray-500">Tag: {product.product_tag}</p>
               </div>
             ) : (
               <img
                 src={guitarImage}
                 alt="Product image"
-                className="w-full h-48 object-cover rounded-t-lg mb-4"
+                className="w-full h-full object-cover rounded-lg mb-4"
               />
             )}
           </div>
